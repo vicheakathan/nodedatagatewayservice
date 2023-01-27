1032a98399736ed2ce0d3c27152f6ad39fdc0b72
than vicheaka <112739297+vicheakathan@users.noreply.github.com>
2023-01-27 15:36:59 +0700

    f

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fs = require('fs');
+var http = require('http');
 const app = express();
 const port = 3000;
+
+app.use(cors({ origin: '*' }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, () => {
   console.log("🎉Server is running🎉");
