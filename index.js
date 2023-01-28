ebc7154629712af008018c134e4343109201c082
than vicheaka <112739297+vicheakathan@users.noreply.github.com>
2023-01-28 10:17:10 +0700

    fix

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ const url = require("url");
 
 // app.use(cors({ origin: '*' }));
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.listen(port, () => {
   console.log("🎉Server is running🎉");
 });
